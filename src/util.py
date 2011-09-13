3a1b68af4c63608082ef16d5d804467e77d2c97a
Felix <felix__@web.de>
2011-09-13 16:12:05 +0200

    load_image should now automatically prepend the image-directory

--- src/util.py
+++ src/util.py
@@ -1,6 +1,10 @@
 import pygame
+from  os.path import join
 
-def load_image(path):
+def load_image(image_filename):
+	path = join("..","images",image_filename)
+		# If the game runs from within the "src"-directory we first have
+		# to go one level up ("..").
 	
 	colorkey = (255, 0, 255)
 	try:
